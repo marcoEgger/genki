f75072512b63afba90163a11b4bad507583dec1f
lukas <lukas.jarosch@mail.com>
2019-11-06 16:06:29 +0100

    add simple static FileServer
    Signed-off-by: lukas <lukas.jarosch@mail.com>

--- server/http/file.go
+++ server/http/file.go
@@ -3,32 +3,28 @@ package http
 import (
 	"net/http"
 	"os"
-	"path"
 
 	"github.com/lukasjarosch/genki/logger"
 )
 
 type swaggerHandler struct {
-	swaggerFile string
+	file string
 }
 
-func NewSwaggerHandler(swaggerFile string) *swaggerHandler {
+func NewFileServer(filePath string) *swaggerHandler {
 	return &swaggerHandler{
-		swaggerFile:swaggerFile,
+		file: filePath,
 	}
 }
 
-// ServeHTTP will serve the swagger file. If the file does not exist, a warning
+// ServeHTTP will serve the file. If the file does not exist, a warning
 // is logged and a 404 returned.
 func (swagger *swaggerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := os.Stat(swagger.swaggerFile); os.IsNotExist(err) {
-		logger.Warnf("cannot serve swagger file '%s': %s", swagger.swaggerFile, err)
+	if _, err := os.Stat(swagger.file); os.IsNotExist(err) {
+		logger.Warnf("cannot serve file '%s': %s", swagger.file, err)
 		http.NotFound(w, r)
 		return
 	}
 
-	logger.Infof("serving %s", r.URL.Path)
-	wd, _ := os.Getwd()
-	swaggerFile := path.Join(wd, swagger.swaggerFile)
-	http.ServeFile(w, r, swaggerFile)
+	http.ServeFile(w, r, swagger.file)
 }
