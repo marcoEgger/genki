9909697c501d4dd123da5d178b6f8363129004ae
lukas <lukas.jarosch@mail.com>
2020-01-14 11:59:43 +0100

    bugfix
    Signed-off-by: lukas <lukas.jarosch@mail.com>

--- types/nullable/int.go
+++ types/nullable/int.go
@@ -27,7 +27,7 @@ func (i Int64) Value() (driver.Value, error) {
 	if !i.Valid {
 		return -1, nil
 	}
-	return int64(i.Int64), nil
+	return i.Int64, nil
 }
 
 type Int32 struct {
@@ -51,7 +51,7 @@ func NewInt32(i int32) Int32 {
 // The integer is converted to an int64 as Sql does not support int32 types
 func (i Int32) Value() (driver.Value, error) {
 	if !i.Valid {
-		return int32(-1), nil
+		return int64(-1), nil
 	}
 	return int64(i.Int32), nil // convert to int64 as mysql does not support int/int32 types
 }
