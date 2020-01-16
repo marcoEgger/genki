bbea106b1ff67931cc4a4212ca2b1478d17ae3f3
lukas <lukas.jarosch@mail.com>
2020-01-16 12:16:40 +0100

    add NewPanicRecoveredError
    Signed-off-by: lukas <lukas.jarosch@mail.com>

--- types/error.go
+++ types/error.go
@@ -1,5 +1,12 @@
 package types
 
+import "fmt"
+
 // PanicRecoveredError can be used as default error type when panics have been recovered.
 // This can come in handy if using a recovery middleware or simply while encoding errors to transport layer.
 type PanicRecoveredError struct{ error }
+
+// NewPanicRecoveredError wraps the error as PanicRecoveredError and adds 'recovered panic:' to the message.
+func NewPanicRecoveredError(err error) PanicRecoveredError {
+	return PanicRecoveredError{fmt.Errorf("recovered panic: %s", err)}
+}
