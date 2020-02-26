0cbaf81e9bf161fd8fd945b2ac5e1f515790519f
lukas <lukas.jarosch@mail.com>
2020-02-26 11:17:45 +0100

    cleanup
    Signed-off-by: lukas <lukas.jarosch@mail.com>

--- broker/broker.go
+++ broker/broker.go
@@ -37,34 +37,3 @@ type Event interface {
 	RoutingKey() string
 	SetContext(ctx context.Context)
 }
-
-/*
-type SubscriptionCreator interface {
-	AddSubscription(exchangeName, queueName, routingKey string, handler Subscriber) error
-}
-
-type PublishCreator interface {
-	AddPublisher(exchangeName, routingKey string) error
-}
-
-type Broker interface {
-	PublishProvider
-	SubscriptionCreator
-	PublishCreator
-	Declare() error
-	Consume(wg *sync.WaitGroup)
-	Shutdown()
-}
-
-type Subscriber func(ctx context.Context, delivery amqp.Delivery)
-
-type Publisher func(ctx context.Context, routingKey string, event interface{}) error
-
-type PublishProvider interface {
-	Publish(ctx context.Context, routingKey string, event interface{}) error
-}
-
-type PublishInterceptor func(next Publisher) Publisher
-type SubscriptionInterceptor func(next Subscriber) Subscriber
-
-*/
