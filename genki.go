f447985aa98cc047e641b0e43e278a462b87a9f8
Marco Egger <marco.egger.me@gmail.com>
2021-11-26 17:25:37 +0100

    Adapted App interface.

--- genki.go
+++ genki.go
@@ -3,6 +3,8 @@ package genki
 import (
 	"github.com/marcoEgger/genki/broker"
 	"github.com/marcoEgger/genki/server"
+	"github.com/marcoEgger/genki/server/http"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 // Application defines the application interface. It's designed to be simple and straightforward.
@@ -11,11 +13,13 @@ type Application interface {
 	Name() string
 	// Run the application. This is a blocking call and will only return if the
 	// server is shut-down or an error occurred.
-	Run() error
+	Run(healthServer *grpc_health_v1.HealthServer) error
 	// Opts returns the current options
 	Opts() Options
 	// AddServer registers a new server with the application
 	AddServer(server server.Server)
+	// AddHttpServer registers a new http server with the application
+	AddHttpServer(srv http.Server)
 	// RegisterBroker registers a message broker implementation (AMQP, NATS, ...)
 	RegisterBroker(broker broker.Broker)
 }
