6567d0f56cea9cfe4d02a4f56f9940d77bdaf907
Marco Egger <marco.egger.me@gmail.com>
2023-08-29 08:31:56 +0200

    Added custom trace sampler to exclude ping traces from health check.

--- tracing/trace.go
+++ tracing/trace.go
@@ -7,7 +7,21 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/semconv/v1.12.0"
+	"strings"
 )
+
+type CustomSampler struct{}
+
+func (cs *CustomSampler) ShouldSample(p trace.SamplingParameters) trace.SamplingResult {
+	if strings.Contains(p.Name, "ping") {
+		return trace.SamplingResult{Decision: trace.Drop}
+	}
+	return trace.SamplingResult{Decision: trace.RecordAndSample}
+}
+
+func (cs *CustomSampler) Description() string {
+	return "CustomSampler"
+}
 
 //goland:noinspection GoUnusedExportedFunction
 func InitTracing(service string, namespace string, url string) error {
@@ -16,11 +30,15 @@ func InitTracing(service string, namespace string, url string) error {
 	if err != nil {
 		return err
 	}
-	traceProvider := trace.NewTracerProvider(trace.WithBatcher(exporter), trace.WithResource(resource.NewWithAttributes(
-		semconv.SchemaURL,
-		semconv.ServiceNameKey.String(service),
-		semconv.ServiceNamespaceKey.String(namespace),
-	)))
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(service),
+			semconv.ServiceNamespaceKey.String(namespace),
+		)),
+		trace.WithSampler(&CustomSampler{}),
+	)
 	otel.SetTracerProvider(traceProvider)
 	otel.SetTextMapPropagator(b3.New())
 	return nil
