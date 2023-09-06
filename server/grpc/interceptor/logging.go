189498343bcbe399330f2552ffbc5de78f8e0765
Marco Egger <marco.egger.me@gmail.com>
2023-09-06 11:59:51 +0200

    Reduced gRPC client logs from info to debug.

--- server/grpc/interceptor/logging.go
+++ server/grpc/interceptor/logging.go
@@ -47,7 +47,7 @@ func UnaryServerLogging() grpc.UnaryServerInterceptor {
 func UnaryClientLogging() grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) (err error) {
 		log := logger.WithMetadata(ctx)
-		log.Infof("client call '%s' to server '%s'", method, cc.Target())
+		log.Debugf("client call '%s' to server '%s'", method, cc.Target())
 		defer func(started time.Time) {
 			log = log.WithFields(logger.Fields{
 				"took": time.Since(started),
@@ -61,13 +61,13 @@ func UnaryClientLogging() grpc.UnaryClientInterceptor {
 						"status":  grpcStatus.Code().String(),
 						"details": grpcStatus.Details(),
 					})
-					log.Infof("finished unary request to: '%s' with status '%s' and message: '%s'", method, grpcStatus.Code().String(), grpcStatus.Message())
+					log.Debugf("finished unary request to: '%s' with status '%s' and message: '%s'", method, grpcStatus.Code().String(), grpcStatus.Message())
 					return
 				}
 
 				log.Errorf("client call to '%s' (server=%s) failed: %s", method, cc.Target(), err)
 			} else {
-				log.Infof("client request to '%s' was successfully handled by server '%s'", method, cc.Target())
+				log.Debugf("client request to '%s' was successfully handled by server '%s'", method, cc.Target())
 			}
 		}(time.Now())
 
