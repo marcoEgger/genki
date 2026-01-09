e62cf7867af4b16df1f0fe72e4b5574671e1708a
Marco Egger <marco.egger.me@gmail.com>
2026-01-09 15:41:43 +0100

    Vendoring of dependencies.

--- go.mod
+++ go.mod
@@ -55,8 +55,6 @@ require (
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/klauspost/compress v1.18.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
@@ -67,7 +65,6 @@ require (
 	github.com/prometheus/procfs v0.19.2 // indirect
 	github.com/sagikazarmark/locafero v0.12.0 // indirect
 	github.com/signalfx/splunk-otel-go/instrumentation/internal v1.29.0 // indirect
-	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
@@ -82,7 +79,6 @@ require (
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.39.0 // indirect
 	go.opentelemetry.io/otel/metric v1.39.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.9.0 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.3 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
@@ -92,5 +88,4 @@ require (
 	golang.org/x/sys v0.40.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20251222181119-0a764e51fe1b // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- vendor/go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/internal/semconv/event_monitor.go
+++ vendor/go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo/internal/semconv/event_monitor.go
@@ -12,18 +12,15 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/event"
-
 	"go.opentelemetry.io/otel/attribute"
-
 	semconv1210 "go.opentelemetry.io/otel/semconv/v1.21.0"
-	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 )
 
 // Constants for environment variable keys and versions.
 const (
-	semconvOptIn     = "OTEL_SEMCONV_STABILITY_OPT_IN"
-	semconvOptInDup  = "database/dup"
-	semconvOptIn1260 = "database"
+	semconvOptIn    = "OTEL_SEMCONV_STABILITY_OPT_IN"
+	semconvOptInDup = "database/dup"
 )
 
 // EventMonitor is responsible for monitoring events with a specified semantic
@@ -90,13 +87,7 @@ func (m EventMonitor) CommandStartedTraceAttrs(
 		)
 	}
 
-	// Check for the 1.26.0 opt-in
-	if hasOptIn(m.version, semconvOptIn1260) {
-		return commandStartedTraceAttrs(evt, opts...)
-	}
-
-	// Fallback to v1.21.0
-	return commandStartedTraceAttrsV1210(evt, opts...)
+	return commandStartedTraceAttrs(evt, opts...)
 }
 
 // peerInfo extracts the hostname and port from a CommandStartedEvent.
@@ -137,13 +128,19 @@ func commandStartedTraceAttrs(evt *event.CommandStartedEvent, setters ...Attribu
 
 	attrs := []attribute.KeyValue{semconv.DBSystemNameMongoDB}
 
-	attrs = append(attrs, semconv.DBOperationName(evt.CommandName))
-	attrs = append(attrs, semconv.DBNamespace(evt.DatabaseName))
-	attrs = append(attrs, semconv.NetworkTransportTCP)
+	attrs = append(
+		attrs,
+		semconv.DBOperationName(evt.CommandName),
+		semconv.DBNamespace(evt.DatabaseName),
+		semconv.NetworkTransportTCP,
+	)
 
 	hostname, port := peerInfo(evt)
-	attrs = append(attrs, semconv.NetworkPeerPort(port))
-	attrs = append(attrs, semconv.NetworkPeerAddress(net.JoinHostPort(hostname, strconv.Itoa(port))))
+	attrs = append(
+		attrs,
+		semconv.NetworkPeerPort(port),
+		semconv.NetworkPeerAddress(net.JoinHostPort(hostname, strconv.Itoa(port))),
+	)
 
 	if !opts.commandAttributeDisabled {
 		attrs = append(attrs, semconv.DBQueryText(sanitizeCommand(evt.Command)))
@@ -166,13 +163,19 @@ func commandStartedTraceAttrsV1210(evt *event.CommandStartedEvent, setters ...At
 
 	attrs := []attribute.KeyValue{semconv1210.DBSystemMongoDB}
 
-	attrs = append(attrs, semconv1210.DBOperation(evt.CommandName))
-	attrs = append(attrs, semconv1210.DBName(evt.DatabaseName))
-	attrs = append(attrs, semconv1210.NetTransportTCP)
+	attrs = append(
+		attrs,
+		semconv1210.DBOperation(evt.CommandName),
+		semconv1210.DBName(evt.DatabaseName),
+		semconv1210.NetTransportTCP,
+	)
 
 	hostname, port := peerInfo(evt)
-	attrs = append(attrs, semconv1210.NetPeerPort(port))
-	attrs = append(attrs, semconv1210.NetPeerName(hostname))
+	attrs = append(
+		attrs,
+		semconv1210.NetPeerPort(port),
+		semconv1210.NetPeerName(hostname),
+	)
 
 	if !opts.commandAttributeDisabled {
 		attrs = append(attrs, semconv1210.DBStatement(sanitizeCommand(evt.Command)))
